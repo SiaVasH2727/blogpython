37299c57e10acf438fe60e452c37ab1745e4480b
SiaVasH <apourrahmati@gmail.com>
2021-01-21 19:22:38 +0330

    Video7 template Run

--- blogpython/settings.py
+++ blogpython/settings.py
@@ -127,8 +127,17 @@ USE_TZ = True
 # CKEditor Settings
 # Static Files
 
-STATIC_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_URL = "/static/"
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static')
+# )
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # CKEditor
 
--- blogpython/urls.py
+++ blogpython/urls.py
@@ -17,11 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from django.conf.urls import include
+from django.conf.urls import include,url
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('ckeditor/', include('ckeditor_uploader.urls')),
-    url(r'^',include('blog.urls'))
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+    url(r'^', include('appblog.urls'))
 ]
